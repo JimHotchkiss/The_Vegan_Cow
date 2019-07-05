6ccdeb8df68c92b3e45454fbc90810c7de34433d
jimhotchkiss <jimbohtochkiss@yahoo.com>
2019-07-05 07:15:46 -0700

    Fixed the JS and CSS animation on the NavBar.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,17 +5,14 @@ const navSlide = () => {
     const nav = document.querySelector('.nav-links');
     // Grab the individual links
     const navLinks = document.querySelectorAll('.nav-links li')
-
     // When we click on the menu, we want the berger to get the class of nav-active, and slide from out of view into view
     burger.addEventListener('click', () => {
       nav.classList.toggle('nav-active');
+      // Animate links - This shit aint working!!
+      navLinks.forEach((link, index) => {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.8}s`;
+        console.log(link)
+      });
     });
-
-    // Animate links - This shit aint working!!
-    
-    navLinks.forEach(function(link, index) {
-    });
-
 }
-
 navSlide();
