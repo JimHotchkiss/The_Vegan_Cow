15ca75fc7ebab5e6a397661ed9f7877553a1299b
github username <github email address>
2019-07-20 15:33:04 -0700

    Add 'description' to recipes, added styling to recipes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_002433) do
+ActiveRecord::Schema.define(version: 2019_07_20_222444) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_07_15_002433) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
 end
