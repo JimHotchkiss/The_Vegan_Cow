bfd09732e43bb1d80b1d1e59f0e42bdf1d7fee4d
github username <github email address>
2019-09-03 18:50:07 -0700

    Added JS Object Model and prototype.

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -7,14 +7,35 @@ const clickHandlers = () => {
   $('#recipe-index').on('click', (event) => {
     event.preventDefault();
     // Now lets get the data through the Fetch API
-
     fetch('/recipes.json')
       .then(response => response.json())
       .then(recipes => {
         // lets clear the app-container
+        $('#app-container').html("")
         recipes.forEach(recipe => {
-          $('#app-container').html(`<a href="#">${recipe.title}</a>`)
+          // Lets build an Object Model
+          let newRecipe = new Recipe(recipe)
+          // Lets call our prototype
+          let recipeHTML = newRecipe.formatIndex()
+          // Lets append the DOM
+          $('#app-container').append(recipeHTML)
         })
       })
   })
+}
+
+// JavaScript Object Model
+function Recipe(recipe) {
+  this.id = recipe.id
+  this.title = recipe.title
+  this.description = recipe.description
+  this.instructions = recipe.instructions
+  this.category = recipe.category
+}
+
+// Recipe prototype
+Recipe.prototype.formatIndex = function(){
+  let recipeHTML = `<a href = "#"><h3>${this.title}</h3></a>`
+
+  return recipeHTML
 }
